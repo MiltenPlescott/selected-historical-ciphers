05c1c1ee106070d67696655c28b2a68b1aa05009
MiltenPlescott <milten.plescott@gmx.com>
2018-07-21 02:53:21 +0200

    Add LICENSE.txt and license headers

--- src/main/java/shc/fersen/attack/Candidate.java
+++ src/main/java/shc/fersen/attack/Candidate.java
@@ -1,3 +1,11 @@
+/*
+ * selected-historical-ciphers
+ * 
+ * Copyright (c) 2018, Milten Plescott. All rights reserved.
+ * 
+ * SPDX-License-Identifier:    BSD-3-Clause
+ */
+
 package shc.fersen.attack;
 
 import java.util.ArrayList;
